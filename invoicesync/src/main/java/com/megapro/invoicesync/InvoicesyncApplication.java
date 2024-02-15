f0b2b1d12d5a73d4977d2df20ef9f2dca73778f9
griseldans <griseldaneysasaadiya@gmail.com>
2024-02-15 19:30:01 +0700

    files: project base

---
+++ invoicesync/src/main/java/com/megapro/invoicesync/InvoicesyncApplication.java
@@ -0,0 +1,13 @@
+package com.megapro.invoicesync;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InvoicesyncApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InvoicesyncApplication.class, args);
+	}
+
+}
