323a7066e8e97db406e065770e3e42d721fcab3b
griseldans <griseldaneysasaadiya@gmail.com>
2024-02-16 17:58:05 +0700

    files: deploy preparation

---
+++ invoicesync/src/main/java/com/megapro/invoicesync/controller/PageController.java
@@ -0,0 +1,13 @@
+package com.megapro.invoicesync.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class PageController {
+    @GetMapping("/")
+    public String home(){
+        return "landing-page";
+    }
+
+}
