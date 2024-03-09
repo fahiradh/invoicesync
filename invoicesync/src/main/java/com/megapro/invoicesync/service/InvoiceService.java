839206fb2e55f8fa783dfce28d4dbc1d2897ca5d
Muhammad Irsyad Mufid <muhammad.irsyad17@ui.ac.id>
2024-03-09 21:26:55 +0700

    Irsyad

--- invoicesync/src/main/java/com/megapro/invoicesync/service/InvoiceService.java
+++ invoicesync/src/main/java/com/megapro/invoicesync/service/InvoiceService.java
@@ -8,16 +8,22 @@ import java.util.UUID;
 
 import com.megapro.invoicesync.dto.response.ReadInvoiceResponse;
 
+import com.megapro.invoicesync.dto.response.ReadInvoiceResponse;
+import com.megapro.invoicesync.model.Invoice;
+import java.util.List;
+import java.util.UUID;
+
 public interface InvoiceService {
     void createInvoice(Invoice invoice, String email);
     void attributeInvoce(Invoice invoice);
     long countInvoice();
     Invoice getInvoiceById(UUID id);
     List<Invoice> retrieveAllInvoice();
-    List<Invoice> getInvoiceByStaffEmail(String email);
-    List<Product> getListProductInvoice(Invoice invoice);
     List<Invoice> retrieveInvoicesByRole(String role);
     List<Invoice> retrieveInvoicesByEmail(String email);
+    public List<Invoice> retrieveInvoicesByDivision(String division);
+    List<Invoice> getInvoiceByStaffEmail(String email);
+    List<Product> getListProductInvoice(Invoice invoice);
     List<Invoice> retrieveInvoicesByEmailAndStatus(String email, String status);
     
 }
